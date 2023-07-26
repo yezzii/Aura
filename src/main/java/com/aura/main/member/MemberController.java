b3bcca5f9da3552c6398506edf54d6aafc8de311
yezzii <118273737+yezzii@users.noreply.github.com>
2023-07-26 21:37:42 +0900

    카카오로그인

--- src/main/java/com/aura/main/member/MemberController.java
+++ src/main/java/com/aura/main/member/MemberController.java
@@ -1,17 +1,17 @@
 package com.aura.main.member;
 
 import com.aura.main.model.MemberDTO;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 
 @Controller
@@ -21,9 +21,13 @@ public class MemberController {
     @Autowired
     private MemberRepository memberRepository;
 
+
     //로그인 페이지
     @GetMapping("/login")
-    public String login() {
+
+
+    public String login(Model model) {
+
         return "member/login";
     }
     //로그인 체크
@@ -49,6 +53,20 @@ public class MemberController {
         session.invalidate();
         return "redirect:/login";
     }
+    // 카카오 API 로그인
+
+    @RestController
+    @AllArgsConstructor
+    @RequestMapping("/kakao")
+    public class kakaoController{
+        @GetMapping("/login")
+        public String kakao(@RequestParam String code,Model model)throws IOException{
+            System.out.println("code = " + code);
+            String access_token = memberService.getToken(code);
 
 
+            return "member/kakaologin";
+        }
     }
+
+}
--- src/main/java/com/aura/main/member/MemberService.java
+++ src/main/java/com/aura/main/member/MemberService.java
@@ -1,10 +1,28 @@
 package com.aura.main.member;
 
 import com.aura.main.model.MemberDTO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.tomcat.util.json.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.http.HttpHeaders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
 
 @Service
 public class MemberService {
@@ -12,8 +30,61 @@ public class MemberService {
     @Autowired
     private MemberRepository memberRepository;
 
-   // 아이디와 비밀번호로 회원 정보를 조회하여 일치하는 회원이 있는지 확인하는 메서드
-    public MemberDTO loginCheck(String memberId,String memberPwd){
-        return memberRepository.findByMemberIdAndMemberPwd(memberId,memberPwd);
+    // 아이디와 비밀번호로 회원 정보를 조회하여 일치하는 회원이 있는지 확인하는 메서드
+    public MemberDTO loginCheck(String memberId, String memberPwd) {
+        return memberRepository.findByMemberIdAndMemberPwd(memberId, memberPwd);
     }
+
+    // 카카오 로그인
+    public String getToken(String code) throws IOException{
+        //인가코드로 토큰 받기
+        String host="https://kauth.kakao.com/oauth/token";
+        URL url = new URL(host);
+        HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+        String token = "";
+        try{
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setDoOutput(true);
+
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(urlConnection.getOutputStream()));
+            StringBuilder sb = new StringBuilder();
+            sb.append("grant_type=authorization_code");
+            sb.append("&client_id=2cba3134e81cfb884bccc490cd964f4a"); //rest값
+            sb.append("&redirect_uri=http://localhost:8989/kakao/login"); //redurect_uri 값
+            sb.append("&code=" + code);
+
+            bw.write(sb.toString());
+            bw.flush();
+
+            int responseCode = urlConnection.getResponseCode();
+            System.out.println("responseCode = " + responseCode);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String line = "";
+            String result = "";
+            while((line = br.readLine()) != null){
+                result += line;
+            }
+            System.out.println("result ="+ result);
+
+            //Json parsing
+            JsonParser parser = new JsonParser();
+            JsonObject elem = (JsonObject)parser.parse(result);
+
+            String access_token = elem.get("access_token").toString();
+            String refresh_token = elem.get("refresh_token").toString();
+            System.out.println("access_token =" + access_token);
+            System.out.println("refresh_token =" + refresh_token);
+
+            token = access_token;
+
+            br.close();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return token;
+    }
+
 }
+
