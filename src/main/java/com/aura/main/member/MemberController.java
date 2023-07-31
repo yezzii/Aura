5ad164e699b8b7710a866d9fde023953282b0ccc
OkayDonkey <122771632+OkayDonkey@users.noreply.github.com>
2023-07-31 09:29:42 +0900

    Feat : 네이버 로그인 API

--- src/main/java/com/aura/main/member/MemberController.java
+++ src/main/java/com/aura/main/member/MemberController.java
@@ -1,24 +1,26 @@
 package com.aura.main.member;
 
 import com.aura.main.model.MemberDTO;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.math.BigInteger;
+import java.net.URLEncoder;
+import java.security.SecureRandom;
+import java.util.HashMap;
 
 
 @Controller
 public class MemberController {
     @Autowired
     private MemberService memberService;
+
     @Autowired
     private MemberRepository memberRepository;
 
@@ -30,11 +32,13 @@ public class MemberController {
     @GetMapping("/member")
     public String getMember(Model model) {
         model.addAttribute("list", memberService.memberList());
-    //로그인 페이지
-    @GetMapping("/login")
-    public String login() {
-        return "member/login";
+        return "member";
     }
+    //로그인 페이지
+//    @GetMapping("/login")
+//    public String login() {
+//        return "member/login";
+//    }
     //로그인 체크
     @PostMapping("/check")
     public String loginCheck(HttpServletRequest request, @RequestParam("id")String memberId, @RequestParam("password")String memberPwd) {
@@ -64,5 +68,57 @@ public class MemberController {
         model.addAttribute("mList", memberService.getMemberProfile(no));
         return "profile";
     }
+
+    @GetMapping("/login")
+    public String naverLogin(HttpSession session, Model model) throws Exception{
+
+        System.out.println("naver.go 진입");
+
+        String clientId = "8ljUOFqbhL8ClN644LoP";//애플리케이션 클라이언트 아이디값";
+        String redirectURI = URLEncoder.encode("http://localhost:8989/login/naverLogin.go", "UTF-8");
+        SecureRandom random = new SecureRandom();
+        String state = new BigInteger(130, random).toString();
+
+        String apiURL = "https://nid.naver.com/oauth2.0/authorize?response_type=code";
+        apiURL += "&client_id=" + clientId;
+        apiURL += "&redirect_uri=" + redirectURI;
+        apiURL += "&state=" + state;
+
+        session.setAttribute("state", state);
+        model.addAttribute("apiURL", apiURL);
+
+        System.out.println("apiURL >>> "+apiURL);
+        System.out.println("stats >>> "+state);
+
+        return "member/login";
+    }
+
+    @GetMapping(value = "/naverLogin.go")
+    public @ResponseBody String naverLoginOk(@RequestParam(value = "code", required = false) String code, Model model,
+    HttpSession session) throws Exception{
+
+        System.out.println("/naverLogin.go  진입");
+
+        String accessToken = memberService.getNaverAccessToken(code, session);
+
+        HashMap<String, Object> userInfo = memberService.getNaverUserInfo(accessToken);
+
+        System.out.println(userInfo);
+
+        return userInfo.toString();
+    }
+
+    @GetMapping("/naverLoginOk.go")
+    public String naverLoginOk(){
+
+        System.out.println("네이버로그인 완료");
+        return "index";
+    }
+
+    public String generateState(){
+        SecureRandom random = new SecureRandom();
+        return new BigInteger(130, random).toString(32);
+    }
+
 
 }
--- src/main/java/com/aura/main/member/MemberService.java
+++ src/main/java/com/aura/main/member/MemberService.java
@@ -1,9 +1,20 @@
 package com.aura.main.member;
 
 import com.aura.main.model.MemberDTO;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,4 +31,115 @@ public class MemberService {
     public MemberDTO loginCheck(String memberId,String memberPwd){
         return memberRepository.findByMemberIdAndMemberPwd(memberId,memberPwd);
     }
-}
+
+    public String getNaverAccessToken(String authorize_code, HttpSession session) throws Exception {
+
+        String access_token = "";
+        String refresh_token = "";
+        String reqURL = "https://nid.naver.com/oauth2.0/token";
+        String clientId = "8ljUOFqbhL8ClN644LoP";
+        String clientSecret = "PLsyV7b8qH";//애플리케이션 클라이언트 시크릿값";
+        String state = (String) session.getAttribute("state");
+        String redirectURI = "http://localhost:8989/login/naverLogin.go";
+
+        // 요청 할 url 객체 생성
+        URL url = new URL(reqURL);
+
+        // URL 연결에 주소값 담긴 객체 전송
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        // 메서드 post 요청
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("grant_type=authorization_code");
+        sb.append("&client_id="+clientId); //본인이 발급받은 key
+        sb.append("&client_secret="+clientSecret); //애플리케이션 클라이언트 시크릿값";
+        sb.append("&redirect_uri="+redirectURI); // 본인이 설정한 주소
+        sb.append("&code=" + authorize_code);
+        sb.append("&state=" + state);
+
+        bw.write(sb.toString());
+        bw.flush();
+
+        int responseCode = conn.getResponseCode();
+
+        System.out.println("responseCode : " + responseCode);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+        String line = "";
+        String result = "";
+
+        while ((line = br.readLine()) != null) {
+            result += line;
+        }
+
+        JsonParser parser = new JsonParser();
+        JsonElement element = parser.parse(result);
+
+        access_token = element.getAsJsonObject().get("access_token").getAsString();
+        refresh_token = element.getAsJsonObject().get("refresh_token").getAsString();
+
+        br.close();
+        bw.close();
+
+        return access_token;
+
+
+    }
+
+    public HashMap<String, Object> getNaverUserInfo(String access_Token) throws Exception {
+
+        HashMap<String, Object> userInfo = new HashMap<String, Object>();
+
+        String reqURL = "https://openapi.naver.com/v1/nid/me";
+
+        URL url = new URL(reqURL);
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Authorization", "Bearer " + access_Token);
+
+        int responseCode = conn.getResponseCode();
+
+        System.out.println("responseCode : " + responseCode);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+        String line = "";
+        String result = "";
+
+        while ((line = br.readLine()) != null) {
+            result += line;
+        }
+
+        System.out.println("response body : " + result);
+
+        JsonParser parser = new JsonParser();
+
+        JsonElement element = parser.parse(result);
+
+        JsonObject response = element.getAsJsonObject().get("response").getAsJsonObject();
+
+        String name = response.getAsJsonObject().get("name").getAsString();
+        String email = response.getAsJsonObject().get("email").getAsString();
+        String phone = response.getAsJsonObject().get("mobile").getAsString();
+        String birthyear = response.getAsJsonObject().get("birthyear").getAsString();
+        String birthday = response.getAsJsonObject().get("birthday").getAsString();
+        String gender = response.getAsJsonObject().get("gender").getAsString();
+
+        userInfo.put("nickname", name);
+        userInfo.put("email", email);
+        userInfo.put("phone", phone);
+        userInfo.put("birthyear", birthyear);
+        userInfo.put("birthday", birthday);
+        userInfo.put("gender", gender);
+
+        return userInfo;
+    }
+
+    }
